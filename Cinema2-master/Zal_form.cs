fb734d26109e20610adf957e9cdbf06395aa5187
Georgi213 <77331773+Georgi213@users.noreply.github.com>
2022-01-21 15:58:37 +0200

    Add files via upload

---
+++ Cinema2-master/Zal_form.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kinoteatr_bilet
+{
+    public partial class Zal_form : Form
+    {
+
+
+
+        public Zal_form()
+        {
+            this.ClientSize = new System.Drawing.Size(400, 350);
+
+            Button mal_btn = new Button
+            {
+                Text = "Väike saal",
+                Location = new System.Drawing.Point(130, 100),//Point(x,y)
+                Height = 50,
+                Width = 150,
+                BackColor = Color.LightGreen
+            };
+            mal_btn.Click += Mal_btn_Click;
+
+            Button sred_btn = new Button
+            {
+                Text = "Keskmine saal",
+                Location = new System.Drawing.Point(130, 170),//Point(x,y)
+                Height = 50,
+                Width = 150,
+                BackColor = Color.LightGreen
+            };
+            sred_btn.Click += Sred_btn_Click;
+
+            Button bol_btn = new Button
+            {
+                Text = "Suur saal",
+                Location = new System.Drawing.Point(130, 240),//Point(x,y)
+                Height = 50,
+                Width = 150,
+                BackColor = Color.LightGreen
+            };
+            bol_btn.Click += Bol_btn_Click;
+
+            Label lbl_zal = new Label
+            {
+                Text = "Valimine kinosaali",
+                Size = new System.Drawing.Size(180, 50),
+                Location = new System.Drawing.Point(135, 25),
+                Font = new Font("Oswald", 16, FontStyle.Bold)
+            };
+
+            this.Controls.Add(lbl_zal);
+            this.Controls.Add(mal_btn);
+            this.Controls.Add(sred_btn);
+            this.Controls.Add(bol_btn);
+            this.BackColor = Color.LightBlue;
+        }
+
+        private void Bol_btn_Click(object sender, EventArgs e)
+        {
+            Zal_vaata uus_aken = new Zal_vaata(9, 9);//запускает пустую форму
+            uus_aken.StartPosition = FormStartPosition.CenterScreen;
+            uus_aken.ShowDialog();
+        }
+
+        private void Sred_btn_Click(object sender, EventArgs e)
+        {
+            Zal_vaata uus_aken = new Zal_vaata(7, 7);//запускает пустую форму
+            uus_aken.StartPosition = FormStartPosition.CenterScreen;
+            uus_aken.ShowDialog();
+        }
+
+        private void Mal_btn_Click(object sender, EventArgs e)
+        {
+            Zal_vaata uus_aken = new Zal_vaata(5, 5);//запускает пустую форму
+            uus_aken.StartPosition = FormStartPosition.CenterScreen;
+            uus_aken.ShowDialog();
+        }
+
+
+        private void MyFotm_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
